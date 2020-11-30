b826dcd6d27f76e9b8999aa5a140e18d4e586bee
s2251605046 <2251605046@qq.com>
2020-11-30 10:17:04 +0800

    hello案例

---
+++ spring-boot-hello/src/main/java/com/sc/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.sc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+    @RequestMapping("/")
+    public String index() {
+        return "Spring Boot 2.0 小案例!";
+    }
+}
